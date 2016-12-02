397f92bc919bc6f81956e9fdb5adaf43645be2dd
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2016-12-02 22:01:59 +0000

    implemented different time format toggle added semicolons

--- src/VueAudio.js
+++ src/VueAudio.js
@@ -1,30 +1,30 @@
 const pad = (val) => {
-    val = Math.floor(val)
+    val = Math.floor(val);
     if (val < 10) {
-        return '0' + val
+        return '0' + val;
     }
-    return val + ''
-}
+    return val + '';
+};
 
 const Cov = {
     on (el, type, func) {
-        el.addEventListener(type, func)
+        el.addEventListener(type, func);
     },
     off (el, type, func) {
         el.removeEventListener(type, func)
     }
-}
+};
 
 class VueAudio {
-    constructor (src, options = {}) {
-        let preload = true
+    constructor(src, options = {}) {
+        let preload = true;
         if (options.preload !== undefined && options.preload === false) {
-            preload = false
+            preload = false;
         }
         this.tmp = {
             src: src,
             options: options
-        }
+        };
         this.state = {
             preload: preload,
             startLoad: false,
@@ -41,137 +41,140 @@ class VueAudio {
             loaded: '0',
             durationTimerFormat: '00:00',
             currentTimeFormat: '00:00',
-            lastTimeFormat: '00:00'
-        }
+            lastTimeFormat: '00:00',
+            timeFormatRemaining: true
+        };
         this.hook = {
             playState: [],
             loadState: []
-        }
+        };
         if (preload) {
-            this.init(src, options)
+            this.init(src, options);
         }
     }
 
-    init (src, options = {}) {
-        if (!src) throw 'src must be required'
-        this.state.startLoad = true
+    init(src, options = {}) {
+        if (!src) throw 'src must be required';
+        this.state.startLoad = true;
         if (this.state.tried === this.state.try) {
-            this.state.failed = true
-            return
+            this.state.failed = true;
+            return;
         }
-        this.$Audio = new window.Audio(src)
+        this.$Audio = new window.Audio(src);
         Cov.on(this.$Audio, 'error', () => {
-            this.state.tried++
+            this.state.tried++;
             this.init(src, options)
-        })
+        });
         if (options.autoplay) {
-            this.play()
+            this.play();
         }
         if (options.rate) {
-            this.$Audio.playbackRate = options.rate
+            this.$Audio.playbackRate = options.rate;
         }
         if (options.loop) {
-            this.$Audio.loop = true
+            this.$Audio.loop = true;
         }
         if (options.volume) {
-            this.setVolume(options.volume)
+            this.setVolume(options.volume);
         }
-        this.loadState()
+        this.loadState();
     }
 
-    loadState () {
+    loadState() {
         if (this.$Audio.readyState >= 2) {
-            Cov.on(this.$Audio, 'progress', this.updateLoadState.bind(this))
+            Cov.on(this.$Audio, 'progress', this.updateLoadState.bind(this));
         } else {
             Cov.on(this.$Audio, 'loadeddata', () => {
-                this.loadState()
+                this.loadState();
             })
         }
     }
 
-    updateLoadState (e) {
-        if (!this.$Audio) return
+    updateLoadState(e) {
+        if (!this.$Audio) return;
         this.hook.loadState.forEach(func => {
-            func(this.state)
-        })
-        this.state.duration = Math.round(this.$Audio.duration * 100) / 100
-        this.state.loaded = Math.round(10000 * this.$Audio.buffered.end(0) / this.$Audio.duration) / 100
-        this.state.durationTimerFormat = this.timeParse(this.state.duration)
+            func(this.state);
+        });
+        this.state.duration = Math.round(this.$Audio.duration * 100) / 100;
+        this.state.loaded = Math.round(10000 * this.$Audio.buffered.end(0) / this.$Audio.duration) / 100;
+        this.state.durationTimerFormat = this.timeParse(this.state.duration);
     }
 
-    updatePlayState (e) {
-        this.state.currentTime = Math.round(this.$Audio.currentTime * 100) / 100
-        this.state.duration = Math.round(this.$Audio.duration * 100) / 100
-        this.state.progress = Math.round(10000 * this.state.currentTime / this.state.duration) / 100
+    updatePlayState(e) {
+        this.state.currentTime = Math.round(this.$Audio.currentTime * 100) / 100;
+        this.state.duration = Math.round(this.$Audio.duration * 100) / 100;
+        this.state.progress = Math.round(10000 * this.state.currentTime / this.state.duration) / 100;
 
-        this.state.durationTimerFormat = this.timeParse(this.state.duration)
-        this.state.currentTimeFormat = this.timeParse(this.state.currentTime)
-        this.state.lastTimeFormat = this.timeParse(this.state.duration - this.state.currentTime)
+        this.state.durationTimerFormat = this.timeParse(this.state.duration);
+        this.state.currentTimeFormat = this.timeParse(this.state.currentTime);
+        if (this.state.timeFormatRemaining) {
+            this.state.lastTimeFormat = '- ' + this.timeParse(this.state.duration - this.state.currentTime);
+        } else {
+            this.state.lastTimeFormat = this.timeParse(this.state.currentTime) + ' / ' + this.timeParse(this.state.duration);
+        }
 
         this.hook.playState.forEach(func => {
             func(this.state)
         })
     }
 
-    updateHook (type, func) {
-        if (!(type in this.hook)) throw 'updateHook: type should be playState or loadState'
+    updateHook(type, func) {
+        if (!(type in this.hook)) throw 'updateHook: type should be playState or loadState';
         this.hook[type].push(func)
     }
 
-    play () {
+    play() {
         if (this.state.startLoad) {
             if (!this.state.playing && this.$Audio.readyState >= 2) {
-                this.$Audio.play()
-                this.state.paused = false
-                this.state.playing = true
+                this.$Audio.play();
+                this.state.paused = false;
+                this.state.playing = true;
                 Cov.on(this.$Audio, 'timeupdate', this.updatePlayState.bind(this))
             } else {
                 Cov.on(this.$Audio, 'loadeddata', () => {
-                    this.play()
+                    this.play();
                 })
             }
         } else {
-            this.init(this.tmp.src, this.tmp.options)
+            this.init(this.tmp.src, this.tmp.options);
             Cov.on(this.$Audio, 'loadeddata', () => {
-                this.play()
+                this.play();
             })
         }
     }
 
-    destroyed () {
-        this.$Audio.pause()
-        Cov.off(this.$Audio, 'timeupdate', this.updatePlayState)
-        Cov.off(this.$Audio, 'progress', this.updateLoadState)
+    destroyed() {
+        this.$Audio.pause();
+        Cov.off(this.$Audio, 'timeupdate', this.updatePlayState);
+        Cov.off(this.$Audio, 'progress', this.updateLoadState);
     }
 
-    pause () {
-        this.$Audio.pause()
-        this.state.paused = true
-        this.state.playing = false
-        this.$Audio.removeEventListener('timeupdate', this.updatePlayState)
+    pause() {
+        this.$Audio.pause();
+        this.state.paused = true;
+        this.state.playing = false;
+        this.$Audio.removeEventListener('timeupdate', this.updatePlayState);
     }
 
-    setVolume (number) {
+    setVolume(number) {
         if (number > -0.01 && number <= 1) {
-            this.state.volume = Math.round(number * 100) / 100
-            this.$Audio.volume = this.state.volume
+            this.state.volume = Math.round(number * 100) / 100;
+            this.$Audio.volume = this.state.volume;
         }
     }
 
-    setTime (time) {
-        if (time < 0 && time > this.state.duration) {
-            return false
-        }
-        this.$Audio.currentTime = time
+    setTime(time) {
+        time = time < 0 ? 0 : time;
+        time = time > this.state.duration ? this.state.duration : time;
+        this.$Audio.currentTime = time;
     }
 
-    timeParse (sec) {
-        let min = 0
-        min = Math.floor(sec / 60)
-        sec = sec - min * 60
-        return pad(min) + ':' + pad(sec)
+    timeParse(sec) {
+        let min = Math.floor(sec / 60);
+        sec = sec - min * 60;
+        return pad(min) + ':' + pad(sec);
     }
 
 }
 
-export default VueAudio
+export default VueAudio;
