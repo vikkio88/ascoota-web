b7f1183194292ce3cffbcd2251253f67249e4232
Vincenzo Ciaccio <vincenzo.ciaccio@sykescottages.co.uk>
2016-12-02 16:09:40 +0000

    project init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ascoota-web
 FE using ascoota-api
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <audio-player :audio="audio" />
+  </div>
+</template>
+
+<script>
+import AudioPlayer from './components/AudioPlayer'
+
+export default {
+  name: 'app',
+  data () {
+        return {
+            audio: {
+                title: 'ninelie-Aimer',
+                src: 'http://covteam.u.qiniudn.com/test2.mp3',
+                poster: 'http://covteam.u.qiniudn.com/ka2.jpg',
+                options: {
+                    preload: false,
+                    autoplay: false,
+                    rate: 1,
+                    loop: false,
+                    volume: 0.5
+                }
+            }
+        }
+    },
+  components: {
+    AudioPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
