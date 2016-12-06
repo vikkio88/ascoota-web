97b7438fda87d757176364d86f452b1db701ea00
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2016-12-06 21:17:28 +0000

    added flag-icon on radio component

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import bootstrapjs from 'bootstrap/dist/js/bootstrap.js'
 import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import bootstrapMaterial from 'bootstrap-material-design/dist/css/bootstrap-material-design.css'
 import bootstrapMaterialJs from 'bootstrap-material-design/dist/js/material.js'
+import flagIconCss from 'flag-icon-css/css/flag-icon.css'
 
 $.material.init();
 
