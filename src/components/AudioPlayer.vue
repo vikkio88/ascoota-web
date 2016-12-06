8825e72d3c7b92381311fdf0a57a99d9f59d502d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2016-12-06 21:47:55 +0000

    small transiction on component entering the dom

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -11,57 +11,69 @@
     .clickable {
         cursor: pointer;
     }
+    
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .3s
+    }
+    
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0
+    }
 </style>
 
 <template>
-    <div class="podcast panel panel-inverse" v-if="audio != undefined">
-        <div class="clearfix" :class="{'panel-heading':showInfo}">
-            <div class="panel-title pull-right">
-                <a @click="toggleInfo" class="clickable">
-                    <i v-show="showInfo" class="material-icons" title="Hide info">expand_less</i>
-                    <i v-show="!showInfo" class="material-icons" title="Show info">expand_more</i>
-                </a>
+    <transition name="fade">
+        <div class="podcast panel panel-inverse" v-if="audio != undefined">
+            <div class="clearfix" :class="{'panel-heading':showInfo}">
+                <div class="panel-title pull-right">
+                    <a @click="toggleInfo" class="clickable">
+                        <i v-show="showInfo" class="material-icons" title="Hide info">expand_less</i>
+                        <i v-show="!showInfo" class="material-icons" title="Show info">expand_more</i>
+                    </a>
+                </div>
             </div>
-        </div>
-        <article class="player-wrapper">
-            <div class="info-wrapper">
-                <div class="content">
-                    <div v-show="showInfo" class="podcast-title">
-                        <h2><strong>{{audio.title}}</strong></h1>
-                    </div>
-                    <div v-show="showInfo" class="podcast-description">
-                        <h3>{{audio.description}}</h3>
-                    </div>
-                    <!--
+            <article class="player-wrapper">
+                <div class="info-wrapper">
+                    <div class="content">
+                            <div v-show="showInfo" class="podcast-title">
+                                <h2><strong>{{audio.title}}</strong></h1>
+                            </div>
+                            <div v-show="showInfo" class="podcast-description">
+                                <h3>{{audio.description}}</h3>
+                            </div>
+                        <!--
                     <div class="audio-slider">
                         <input class="form-control slider" type="range" :value="mu.state.progress" min="0" max="100" step="0.01" v-model="mu.state.progress"
                             @click="seek">
                     </div>
                     -->
-                    <div id="slider-control" @click="seek" class="progress clickable" :class="{'progress-striped':state.playing, 'active': state.playing}"
-                        :style="{ height: showInfo? '10px':'30px'}">
-                        <!--<div v-if="!showInfo" style="width: 100%; z-index:1"><strong>{{audio.title}}</strong> {{mu.state.lastTimeFormat}} / {{mu.state.durationParsed}}</div>-->
-                        <div class="progress-bar" :style="{width: mu.state.progress+'%'}"></div>
+                        <div id="slider-control" @click="seek" class="progress clickable" :class="{'progress-striped':state.playing, 'active': state.playing}"
+                            :style="{ height: showInfo? '10px':'30px'}">
+                            <!--<div v-if="!showInfo" style="width: 100%; z-index:1"><strong>{{audio.title}}</strong> {{mu.state.lastTimeFormat}} / {{mu.state.durationParsed}}</div>-->
+                            <div class="progress-bar" :style="{width: mu.state.progress+'%'}"></div>
+                        </div>
+                        <div v-show="showInfo" class="audio-time" :class="{'clickable': state.playing}" @click="toggleTimeFormat">
+                            <h2>{{mu.state.lastTimeFormat}} / {{mu.state.durationParsed}}</h2>
+                        </div>
                     </div>
-                    <div v-show="showInfo" class="audio-time" :class="{'clickable': state.playing}" @click="toggleTimeFormat">
-                        <h2>{{mu.state.lastTimeFormat}} / {{mu.state.durationParsed}}</h2>
-                    </div>
-                </div>
-                <div class="controls-wrapper">
-                    <button :disabled="!state.playing" @click="skip(-10)" class="btn btn-lg btn-raised" :class="{'disabled': !state.playing}">
+                    <div class="controls-wrapper">
+                        <button :disabled="!state.playing" @click="skip(-10)" class="btn btn-lg btn-raised" :class="{'disabled': !state.playing}">
                         <i class="material-icons">fast_rewind</i>
                     </button>
-                    <button @click="togglePlay" class="btn btn-lg btn-raised btn-primary">
+                        <button @click="togglePlay" class="btn btn-lg btn-raised btn-primary">
                         <i v-show="!state.playing" class="material-icons">play_arrow</i>
                         <i v-show="state.playing" class="material-icons">pause</i>
                     </button>
-                    <button :disabled="!state.playing" @click="skip(10)" class="btn btn-lg btn-raised" :class="{'disabled': !state.playing}">
+                        <button :disabled="!state.playing" @click="skip(10)" class="btn btn-lg btn-raised" :class="{'disabled': !state.playing}">
                         <i class="material-icons">fast_forward</i>
                     </button>
+                    </div>
                 </div>
-            </div>
-        </article>
-    </div>
+            </article>
+        </div>
+    </transition>
 </template>
 
 <script>
