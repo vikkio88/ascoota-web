4da361242b33562875ea6d21225e9b94b38ffce9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-04 20:35:11 +0000

    implemented card view for shows in a Radio details view

---
+++ src/components/podcast/ShowCard.vue
@@ -0,0 +1,30 @@
+<style>
+    .img {
+        height: 180px;
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+<template>
+    <div class="radio">
+        <div class="card">
+            <h3>{{show.name}}</h3>
+            <router-link :to="{ name: 'singleShowView', params: { radioId: show.radio_id , showId: show.id }}" class="btn btn-primary">
+                    <i class="material-icons">zoom_in</i>
+            </router-link>
+        </div>
+    </div>
+</template>
+<script>
+    import FlagIcon from '../common/FlagIcon';
+
+    export default {
+        name: "radio",
+        components: {
+            FlagIcon
+        },
+        props: {
+            show: Object
+        }
+    }
+</script>
--- src/components/podcast/SingleRadio.vue
+++ src/components/podcast/SingleRadio.vue
@@ -1,32 +1,34 @@
 <style></style>
 <template>
     <div class="jumbotron">
-        <h2>{{radio.name}} <flag-icon :language="language" /></h2>
+        <h2>{{radio.name}}
+            <flag-icon :language="language" />
+        </h2>
         <h3>{{radio.description}}</h3>
-        
         <router-link :to="{name:'dashboard'}" class="btn btn-primary">
             <i class="material-icons">dashboard</i>
         </router-link>
-
-        <router-link :to="{name:'radiosShowOne', params : {'showId': 1 }}" class="btn btn-raised">
-            <i class="material-icons">dashboard</i>
-        </router-link>
-
-        <router-view></router-view>
+        <div class="row">
+            <div v-for="show in radio.shows" class="col-sm-4">
+                <show-card :show="show" />
+            </div>
+        </div>
     </div>
 </template>
 <script>
     import FlagIcon from '../common/FlagIcon';
-    import RadioService from '../../services/ascoota/RadioService'
+    import ShowCard from './ShowCard';
+    import RadioService from '../../services/ascoota/RadioService';
 
     var radioService = new RadioService();
 
     export default {
         name: 'single-radio',
         components: {
-            FlagIcon
+            FlagIcon,
+            ShowCard
         },
-        mounted () {
+        mounted() {
             radioService.getOne(this.$route.params.radioId).then(
                 (data) => {
                     this.radio = data.body.payload;
@@ -36,9 +38,9 @@
                 (error) => {
                     console.log(error)
                 }
-            )
+                )
         },
-        data () {
+        data() {
             return {
                 radio: {},
                 language: 'it'
