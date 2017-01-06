169764b29fae34c7c30ce55a5c75614588b30981
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-06 22:35:29 +0000

    added another podcast

--- src/mocks/bazingas.js
+++ src/mocks/bazingas.js
@@ -3,7 +3,7 @@ var bazingas = {
 
         name: 'Bazinga! RadioSisma',
         file_url: 'https://archive.org/download/1BazingaVenerdi27.04.2012/1%20Bazinga%20-%20Venerdi27.04.2012.mp3',
-        description: '1. Puntata 27/04/2012',
+        description: 'Puntata podcast numero 1',
         date: '27/04/2012',
         duration: '02:00:00'
     },
@@ -11,8 +11,16 @@ var bazingas = {
 
         name: 'Bazinga! RadioSisma',
         file_url: 'https://archive.org/download/2BazingaVenerdi11.05.2012/2)%20Bazinga!%20-%20Venerd%C3%AC%20-%2011.05.2012.mp3',
-        description: '2. Puntata 11/05/2012',
+        description: 'Puntata podcast numero 2',
         date: '11/05/2012',
+        duration: '02:00:00'
+    },
+    3: {
+
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/3BazingaVenerdi18.05.2012/3)%20Bazinga!%20-%20Venerd%C3%AC%20-%2018.05.2012.mp3',
+        description: 'Puntata podcast numero 3',
+        date: '18/05/2012',
         duration: '02:00:00'
     },
     4: {
