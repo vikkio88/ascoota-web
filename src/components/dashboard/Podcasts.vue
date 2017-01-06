227f4a86e35f49ac225ce3b162cd70b3e52e000a
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-06 22:34:52 +0000

    moved podcasts dash from panels to cards

--- src/components/dashboard/Podcasts.vue
+++ src/components/dashboard/Podcasts.vue
@@ -6,19 +6,23 @@
         <div class="panel-heading">
             <h3><i class="material-icons">trending_up</i> <i class="material-icons">audiotrack</i></h3>
         </div>
-        <div class="panel-group">
-            <single-podcast :podcast="podcast" v-for="podcast in podcasts" />
+        <div class="panel-body">
+            <div class="row">
+                <div v-for="podcast in podcasts" class="col-sm-4">
+                    <podcast-card :podcast="podcast" />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
-    import SinglePodcast from '../podcast/SinglePodcast'
+    import PodcastCard from '../podcast/cards/PodcastCard'
     import bazingas from '../../mocks/bazingas'
 
     export default {
         name: "podcasts",
         components: {
-            SinglePodcast
+            PodcastCard
         },
         data() {
             return {
