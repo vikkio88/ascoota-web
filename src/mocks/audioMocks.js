c332fc3a1d6017441985b8ee508f2ebbc06eafa3
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-06 22:01:30 +0000

    moving mocks

--- src/mocks/audioMocks.js
+++ src/mocks/audioMocks.js
@@ -19,8 +19,21 @@ var audioMocks = {
 
         name: 'Bazinga! RadioSisma',
         file_url: 'http://www.vikkio.it/ascootaweb/bazinga/4.mp3',
-        description: 'Una puntata tra Trash e Voodoo'
+        description: '4. Una puntata tra Trash e Voodoo - '
+    },
+    5: {
+
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/1BazingaVenerdi27.04.2012/1%20Bazinga%20-%20Venerdi27.04.2012.mp3',
+        description: '1. Puntata 27/04/2012'
+    },
+    6: {
+
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/2BazingaVenerdi11.05.2012/2)%20Bazinga!%20-%20Venerd%C3%AC%20-%2011.05.2012.mp3',
+        description: '2. Puntata 11/05/2012'
     }
+
 };
 
 export default audioMocks;
---
+++ src/mocks/bazingas.js
@@ -0,0 +1,28 @@
+var bazingas = {
+    1: {
+
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/1BazingaVenerdi27.04.2012/1%20Bazinga%20-%20Venerdi27.04.2012.mp3',
+        description: '1. Puntata 27/04/2012',
+        date: '27/04/2012',
+        duration: '02:00:00'
+    },
+    2: {
+
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/2BazingaVenerdi11.05.2012/2)%20Bazinga!%20-%20Venerd%C3%AC%20-%2011.05.2012.mp3',
+        description: '2. Puntata 11/05/2012',
+        date: '11/05/2012',
+        duration: '02:00:00'
+    },
+    4: {
+
+        name: 'Bazinga! RadioSisma',
+        file_url: 'http://www.vikkio.it/ascootaweb/bazinga/4.mp3',
+        description: '4. Una puntata tra Trash e Voodoo',
+        date: '01/06/2012',
+        duration: '02:20:00'
+    }
+};
+
+export default bazingas;
