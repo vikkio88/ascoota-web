52cefd10a5ebb191fa24fc0ce52a46c4c9324c25
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-07 17:29:56 +0000

    add more Bazinga podcasts

--- src/mocks/bazingas.js
+++ src/mocks/bazingas.js
@@ -1,6 +1,6 @@
 var bazingas = {
     1: {
-
+        id: 9991,
         name: 'Bazinga! RadioSisma',
         file_url: 'https://archive.org/download/1BazingaVenerdi27.04.2012/1%20Bazinga%20-%20Venerdi27.04.2012.mp3',
         description: 'Puntata podcast numero 1',
@@ -8,7 +8,7 @@ var bazingas = {
         duration: '02:00:00'
     },
     2: {
-
+        id: 9992,
         name: 'Bazinga! RadioSisma',
         file_url: 'https://archive.org/download/2BazingaVenerdi11.05.2012/2)%20Bazinga!%20-%20Venerd%C3%AC%20-%2011.05.2012.mp3',
         description: 'Puntata podcast numero 2',
@@ -16,7 +16,7 @@ var bazingas = {
         duration: '02:00:00'
     },
     3: {
-
+        id: 9993,
         name: 'Bazinga! RadioSisma',
         file_url: 'https://archive.org/download/3BazingaVenerdi18.05.2012/3)%20Bazinga!%20-%20Venerd%C3%AC%20-%2018.05.2012.mp3',
         description: 'Puntata podcast numero 3',
@@ -24,11 +24,43 @@ var bazingas = {
         duration: '02:00:00'
     },
     4: {
-
+        id: 9994,
         name: 'Bazinga! RadioSisma',
         file_url: 'http://www.vikkio.it/ascootaweb/bazinga/4.mp3',
-        description: '4. Una puntata tra Trash e Voodoo',
+        description: '4. Una puntata tra Voodoo ...',
         date: '01/06/2012',
+        duration: '02:20:00'
+    },
+    5: {
+        id: 9995,
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/5BazingaVenerdi22.06.2012/5)%20Bazinga!%20-%20Venerd%C3%AC%20-%2022.06.2012.mp3',
+        description: 'Puntata podcast numero 5',
+        date: '22/06/2012',
+        duration: '02:20:00'
+    },
+    6: {
+        id: 9996,
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/6Bazinga14.09.2012Venerdi/6)%20Bazinga!%20-%2014.09.2012%20-%20Venerd%C3%AC.mp3',
+        description: 'Puntata podcast numero 6',
+        date: '14/09/2012',
+        duration: '02:20:00'
+    },
+    7: {
+        id: 9997,
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/7Bazinga21.12.2012Venerdi/7)%20Bazinga!%20-%2021.12.2012%20-%20Venerd%C3%AC.mp3',
+        description: 'Puntata podcast numero 7',
+        date: '21/12/2012',
+        duration: '02:20:00'
+    },
+    8: {
+        id: 9998,
+        name: 'Bazinga! RadioSisma',
+        file_url: 'https://archive.org/download/8Bazinga29.03.2013Venerdi/8)%20Bazinga!%20-%2029.03.2013%20-%20Venerd%C3%AC.mp3',
+        description: 'Puntata podcast numero 8',
+        date: '29/03/2013',
         duration: '02:20:00'
     }
 };
