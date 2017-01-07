e17395cef33815148b20be9f8bc9c706c16ac60e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-07 17:28:47 +0000

    removed mocks dependencies

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,6 @@
     <main-nav />
     <div class="container container-fluid">
       <audio-player :audio="audio" />
-      <button class="btn btn-raised btn-xs" @click=test(1)>2024 02/12/2016</button>
-      <button class="btn btn-raised btn-xs" @click=test(2)>La Zanzara 29/11/2016</button>
-      <button class="btn btn-raised btn-xs" @click=test(3)>Too Long - Daft Punk</button>
-      <button class="btn btn-raised btn-xs" @click=test(4)>Bazinga! 01/06/2012</button>
       <router-view></router-view>
     </div>
   </div>
@@ -25,9 +21,6 @@
 import AudioPlayer from './components/AudioPlayer'
 import MainNav from './components/MainNav'
 import Dashboard from './components/Dashboard'
-
-// mocks 
-import audioMocks from './mocks/audioMocks'
 
 export default {
   name: 'app',
@@ -40,11 +33,6 @@ export default {
     AudioPlayer,
     MainNav,
     Dashboard
-  },
-  methods: {
-    test(number){
-      this.audio = audioMocks[number];
-    }
   }
 }
 </script>
