d102c080caee13ccd213b88415b8d75e7bd2faab
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-08 20:02:35 +0000

    changed layout to panels

--- src/components/podcast/Show.vue
+++ src/components/podcast/Show.vue
@@ -1,77 +1,84 @@
 <style></style>
 <template>
-    <div class="jumbotron">
+    <div class="panel panel-primary">
         <img class="img" :alt="show.name" :title="show.name" :src="show.logo_url" />
-        <h2>
-            {{show.name}}
-            <flag-icon :language="language" />
-        </h2>
-        <p>
-            <h3 v-if="show.author">{{show.author}}</h3>
-            <h3>{{show.description}}</h3>
-            <a class="btn btn-primary" :href="show.website" target="_blank">
-                <i class="material-icons">link</i>
-            </a>
-            <a class="btn btn-primary" :href="show.feed_url" target="_blank">
-                <i class="material-icons">rss_feed</i>
-            </a>
-        </p>
-        <div class="jumbotron">
-            <h2>Podcasts</h2>
-            <div v-for="podcast in podcasts" class="panel panel-default">
+        <div class="panel-heading">
+            <h2>
+                {{show.name}}
+                <flag-icon :language="language" />
+            </h2>
+        </div>
+        <div class="panel-body">
+            <p>
+                <h3 v-if="show.author">{{show.author}}</h3>
+                <h3>{{show.description}}</h3>
+                <a class="btn btn-primary" :href="show.website" target="_blank">
+                    <i class="material-icons">link</i>
+                </a>
+                <a class="btn btn-primary" :href="show.feed_url" target="_blank">
+                    <i class="material-icons">rss_feed</i>
+                </a>
+            </p>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3>Podcasts</h3>
+                </div>
                 <div class="panel-body">
-                    <h4>{{podcast.date}}</h4>
-                    <h3>{{podcast.name}}</h3>
-                    <h4>{{podcast.description}}</h4>
-                    <h3>{{podcast.duration}}</h3>
+                    <div class="row">
+                        <div v-for="podcast in podcasts" class="col-sm-4">
+                            <podcast-card :podcast="podcast" :showimg="false" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
-import ShowService from '../../services/ascoota/ShowService.js'
-import FlagIcon from '../common/FlagIcon'
+    import PodcastCard from './cards/PodcastCard'
+    import ShowService from '../../services/ascoota/ShowService.js'
+    import FlagIcon from '../common/FlagIcon'
 
-export default {
-    name: 'show',
-    components: {
-        FlagIcon
-    },
-    data () {
-        return {
-            params : this.$route.params,
-            show: {},
-            language: 'it',
-            podcasts: []
-        };
-    },
-    watch : {
-        params() {
+    export default {
+        name: 'show',
+        components: {
+            FlagIcon,
+            PodcastCard
+        },
+        data() {
+            return {
+                params: this.$route.params,
+                show: {},
+                language: 'it',
+                podcasts: []
+            };
+        },
+        watch: {
+            params() {
+                this.init();
+            }
+        },
+        mounted() {
             this.init();
-        }
-    },
-    mounted () {
-        this.init();
-    },
-    methods : {
-        init(){
-            let params = this.$route.params;
-            let service = new ShowService(params.radioId)
-            service.getOne(params.showId).then(
-                (data) => {
-                    this.show = data.body.payload;
-                    if(this.show.language !== undefined){
-                        this.language = this.show.language.iso;
+        },
+        methods: {
+            init() {
+                let params = this.$route.params;
+                let service = new ShowService(params.radioId)
+                service.getOne(params.showId).then(
+                    (data) => {
+                        this.show = data.body.payload;
+                        if (this.show.language !== undefined) {
+                            this.language = this.show.language.iso;
+                        }
+                        this.podcasts = this.show.podcasts;
                     }
-                    this.podcasts = this.show.podcasts;
-                }
-            ).catch(
-                (error) => {
-                    console.log(error);
-                }
-            );
+                ).catch(
+                    (error) => {
+                        console.log(error);
+                    }
+                    );
+            }
         }
     }
-}
 </script>
--- src/components/podcast/SingleRadio.vue
+++ src/components/podcast/SingleRadio.vue
@@ -1,24 +1,27 @@
 <style></style>
 <template>
-    <div class="jumbotron">
-        <router-link :to="{name:'dashboard'}" class="btn btn-primary">
-            <i class="material-icons">dashboard</i>
-        </router-link>
-        <h2>{{radio.name}}
-            <flag-icon :language="language" />
-        </h2>
-
-        <a class="btn btn-primary" :href="radio.website" target="_blank">
-            <i class="material-icons">link</i>
-        </a>
-
+    <div class="panel panel-primary">
         <img class="img" :alt="radio.name" :title="radio.name" :src="radio.logo_url" />
-        <h3>{{radio.description}}</h3>
-        <div class="jumbotron" v-if="shows.length">
-            <h2>Shows</h2>
-            <div class="row">
-                <div v-for="show in shows" class="col-sm-4">
-                    <show-card :show="show" />
+        <div class="panel-heading">
+            <h2>{{radio.name}}
+                <flag-icon :language="language" />
+            </h2>
+        </div>
+        <div class="panel-body">
+            <h3>{{radio.description}}</h3>
+            <a class="btn btn-primary" :href="radio.website" target="_blank">
+                <i class="material-icons">link</i>
+            </a>
+            <div class="panel panel-primary" v-if="shows.length">
+                <div class="panel-heading">
+                    <h3>Shows</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div v-for="show in shows" class="col-sm-4">
+                            <show-card :show="show" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -26,7 +29,7 @@
 </template>
 <script>
     import FlagIcon from '../common/FlagIcon';
-    import ShowCard from './ShowCard';
+    import ShowCard from './cards/ShowCard';
     import RadioService from '../../services/ascoota/RadioService';
 
     var radioService = new RadioService();
@@ -53,7 +56,7 @@
         data() {
             return {
                 radio: {},
-                shows : [],
+                shows: [],
                 language: 'it'
             }
         }
