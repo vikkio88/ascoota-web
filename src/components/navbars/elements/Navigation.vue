e9a2e268c5d9c8168a88af451e12932ee7e2aa44
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-10 19:24:04 +0000

    moved navigation link to icon

--- src/components/navbars/elements/Navigation.vue
+++ src/components/navbars/elements/Navigation.vue
@@ -1,7 +1,12 @@
 <style></style>
 <template>
     <ul class="nav navbar-nav">
-        <li class="active"><a href="javascript:void(0)">Dashboard<div class="ripple-container"></div></a></li>
+        <li>
+            <a  class="btn btn-xs" href="javascript:void(0)">
+                <i class="material-icons">dashboard</i>
+                <div class="ripple-container"></div>
+            </a>
+        </li>
     </ul>
 </template>
 <script>
