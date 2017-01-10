e383d7ff8f4ba721ad91ccca8434441eba7b49e2
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-10 18:52:16 +0000

    handled possible error

--- src/components/podcast/SingleRadio.vue
+++ src/components/podcast/SingleRadio.vue
@@ -6,31 +6,31 @@
             <div class="panel-title pull-left">
                 <router-link :to="{ name: 'dashboard'}" class="btn">
                     <i class="material-icons" style="color: white">arrow_back</i>
-                </router-link>
-            </div>
-            <h2>{{radio.name}}
-                <flag-icon :language="language" />
-            </h2>
+</router-link>
+</div>
+<h2>{{radio.name}}
+    <flag-icon :language="language" />
+</h2>
+</div>
+<div class="panel-body">
+    <h3>{{radio.description}}</h3>
+    <a class="btn btn-primary" :href="radio.website" target="_blank">
+        <i class="material-icons">link</i>
+    </a>
+    <div class="panel panel-primary" v-if="shows.length">
+        <div class="panel-heading">
+            <h3>Shows</h3>
         </div>
         <div class="panel-body">
-            <h3>{{radio.description}}</h3>
-            <a class="btn btn-primary" :href="radio.website" target="_blank">
-                <i class="material-icons">link</i>
-            </a>
-            <div class="panel panel-primary" v-if="shows.length">
-                <div class="panel-heading">
-                    <h3>Shows</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="row">
-                        <div v-for="show in shows" class="col-sm-4">
-                            <show-card :show="show" />
-                        </div>
-                    </div>
+            <div class="row">
+                <div v-for="show in shows" class="col-sm-4">
+                    <show-card :show="show" />
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 </template>
 <script>
     import FlagIcon from '../common/FlagIcon';
@@ -49,8 +49,10 @@
             radioService.getOne(this.$route.params.radioId).then(
                 (data) => {
                     this.radio = data.body.payload;
-                    this.shows = this.radio.shows;
-                    this.language = data.body.payload.language.iso;
+                    this.shows = this.radio.shows || this.shows;
+                    if (this.radio.language !== undefined) {
+                        this.language = this.radio.language.iso;
+                    }
                 }
             ).catch(
                 (error) => {
