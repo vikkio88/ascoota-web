fd8562d17cdedb6536270f6acf5cfa6b6cf4b634
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-10 19:15:05 +0000

    noved links to subfolder inside navbars

---
+++ src/components/navbars/elements/DevLinks.vue
@@ -0,0 +1,37 @@
+<style></style>
+<template>
+    <ul class="nav navbar-nav navbar-right">
+        <li>
+            <a title="about" class="btn btn-xs" @click="showInfo">
+                <i class="material-icons">info_outline</i>
+            </a>
+        </li>
+        <li>
+            <a title="code on Github" target="_blank" href="https://github.com/vikkio88/ascoota-web" class="btn btn-xs">
+                <img src="../../../assets/github-circle.png" alt="github">
+                <div class="ripple-container"></div>
+            </a>
+        </li>
+        <li>
+            <a title="report an Issue" target="_blank" href="https://github.com/vikkio88/ascoota-web/issues/new" class="btn btn-xs">
+                <i class="material-icons">bug_report</i>
+                <div class="ripple-container"></div>
+            </a>
+        </li>
+    </ul>
+
+</template>
+<script>
+    import StatsPush from '../../../services/stats/StatsPush'
+    var stats = new StatsPush();
+
+    export default {
+        name: 'dev-links',
+        methods: {
+            showInfo() {
+                stats.push('clicked_info');
+                this.$store.state.showMainDialog = true;
+            }
+        }
+    }
+</script>
---
+++ src/components/navbars/elements/Navigation.vue
@@ -0,0 +1,11 @@
+<style></style>
+<template>
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="javascript:void(0)">Dashboard<div class="ripple-container"></div></a></li>
+    </ul>
+</template>
+<script>
+    export default {
+        name: 'navigation'
+    }
+</script>
