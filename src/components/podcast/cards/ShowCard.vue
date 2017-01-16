da780c583fa0adc09f363e8b707249936a7c7cc6
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-16 21:33:39 +0000

    removed icon from show card

--- src/components/podcast/cards/ShowCard.vue
+++ src/components/podcast/cards/ShowCard.vue
@@ -7,15 +7,13 @@
 </style>
 <template>
     <div class="radio">
-        <img class="img card-img-top" :alt="show.name" :title="show.name" :src="show.logo_url" />
+        <router-link :to="{ name: 'singleShowView', params: { radioId: show.radio_id , showId: show.id }}">
+            <img class="img card-img-top" :alt="show.name" :title="show.name" :src="show.logo_url"/>
+        </router-link>
         <div class="card">
             <div class="card-block">
                 <h3>{{show.name}}</h3>
                 <h4>{{show.description}}</h4>
-                <router-link :to="{ name: 'singleShowView', params: { radioId: show.radio_id , showId: show.id }}" class="btn btn-primary">
-                    <i class="material-icons">zoom_in</i>
-                </router-link>
-
             </div>
         </div>
     </div>
