fd081ebaedcc9efa70a4317d463c75f789f5ce30
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-16 21:33:26 +0000

    removed icon from radiocard

--- src/components/podcast/cards/RadioCard.vue
+++ src/components/podcast/cards/RadioCard.vue
@@ -7,18 +7,17 @@
 </style>
 <template>
     <div class="card">
-        <img class="img card-img-top" alt="Card image cap" :src="radio.logo_url" />
+        <router-link :to="{ name: 'singleRadioView', params: { radioId: radio.id }}">
+            <img class="img card-img-top" alt="Card image cap" :src="radio.logo_url" />
+        </router-link>
         <div class="card-block">
             <h3 class="card-title">
                 {{radio.name}}
             </h3>
             <p class="card-text">
-                <flag-icon :language="radio.language.iso"> 
+                <flag-icon :language="radio.language.iso" />
                 {{radio.description}}
             </p>
-            <router-link :to="{ name: 'singleRadioView', params: { radioId: radio.id }}" class="btn btn-primary">
-                <i class="material-icons">zoom_in</i>
-            </router-link>
         </div>
     </div>
 </template>
