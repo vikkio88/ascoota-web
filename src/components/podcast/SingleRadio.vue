d0c17eade289541a478d0f5b566bea4bffa0b52e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-16 21:34:46 +0000

    fixed indentation

--- src/components/podcast/SingleRadio.vue
+++ src/components/podcast/SingleRadio.vue
@@ -1,36 +1,37 @@
 <style></style>
 <template>
     <div class="panel panel-primary">
-        <img class="img" :alt="radio.name" :title="radio.name" :src="radio.logo_url" />
+        <img class="img" :alt="radio.name" :title="radio.name" :src="radio.logo_url"/>
         <div class="panel-heading">
             <div class="panel-title pull-left">
                 <router-link :to="{ name: 'dashboard'}" class="btn">
                     <i class="material-icons" style="color: white">arrow_back</i>
-</router-link>
-</div>
-<h2>{{radio.name}}
-    <flag-icon :language="language" />
-</h2>
-</div>
-<div class="panel-body">
-    <h3>{{radio.description}}</h3>
-    <a class="btn btn-primary" :href="radio.website" target="_blank">
-        <i class="material-icons">link</i>
-    </a>
-    <div class="panel panel-primary" v-if="shows.length">
-        <div class="panel-heading">
-            <h3>Shows</h3>
+                </router-link>
+            </div>
+            <h2>
+                {{radio.name}}
+                <flag-icon :language="language" />
+            </h2>
         </div>
         <div class="panel-body">
-            <div class="row">
-                <div v-for="show in shows" class="col-sm-4">
-                    <show-card :show="show" />
+            <h3>{{radio.description}}</h3>
+            <a class="btn btn-primary" :href="radio.website" target="_blank">
+                <i class="material-icons">link</i>
+            </a>
+            <div class="panel panel-primary" v-if="shows.length">
+                <div class="panel-heading">
+                    <h3>Shows</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div v-for="show in shows" class="col-sm-4">
+                            <show-card :show="show"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
 </template>
 <script>
     import FlagIcon from '../common/FlagIcon';
@@ -68,4 +69,5 @@
             }
         }
     }
+
 </script>
