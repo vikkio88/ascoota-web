4cea6e561b62b2cc5912824aac297ea7b74bb9ca
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-26 21:59:38 +0000

    fixed production var

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   SERVICE_URL: '"http://www.vikkio.it/ascoota-api/"',
-  STATS_URL: 'http://www.vikkio.it/ascoota-api/stats"'
+  STATS_URL: '"http://www.vikkio.it/ascoota-api/stats"'
 }
