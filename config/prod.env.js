52fd9d98fc898838f82ceadad213e00e9bffd7aa
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-01-29 12:34:08 +0000

    moved prod config to https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  SERVICE_URL: '"http://www.vikkio.it/ascoota-api/"',
-  STATS_URL: '"http://www.vikkio.it/ascoota-api/stats"'
+  SERVICE_URL: '"https://www.vikkio.it/ascoota-api/"',
+  STATS_URL: '"https://www.vikkio.it/ascoota-api/stats"'
 }
