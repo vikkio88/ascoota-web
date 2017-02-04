94fff3dc44782c0659df2f3aa2657d707f93cbff
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-04 16:27:17 +0000

    implemented list elements for podcast and radio

---
+++ src/components/podcast/listItems/Podcast.vue
@@ -0,0 +1,45 @@
+<style>
+
+</style>
+<template>
+    <md-list-item @click="toggleMe">
+        <md-avatar class="md-avatar-icon">
+            <img alt="podcast.name" :src="podcast.show.logo_url">
+        </md-avatar>
+
+        <div class="md-list-text-container">
+            <span>{{podcast.name}} - {{podcast.description}}</span>
+            <p>{{podcast.date}}</p>
+        </div>
+        <md-button class="md-raised md-icon-button md-list-action" :class="{ 'md-warn': isSelected, 'md-primary':!isSelected }">
+            <md-icon v-if="!isSelected">play_arrow</md-icon>
+            <md-icon v-if="isSelected">stop</md-icon>
+        </md-button>
+    </md-list-item>
+</template>
+<script>
+    export default {
+        name: "podcast-list-item",
+        props: {
+            podcast: Object,
+            showimg: { type: Boolean, default: true }
+        },
+        computed: {
+            isSelected() {
+                let playingAudio = this.$store.state.selectedAudio !== undefined ? this.$store.state.selectedAudio : { id: -1 };
+                return playingAudio.id === this.podcast.id;
+            }
+        },
+        methods: {
+            toggleMe() {
+                if (!this.isSelected) {
+                    this.$store.state.selectedAudio = this.podcast;
+                    this.$store.state.autoPlay = true;
+                } else {
+                    this.$store.state.selectedAudio = undefined
+                }
+            }
+        }
+    }
+
+</script>
---
+++ src/components/podcast/listItems/Radio.vue
@@ -0,0 +1,35 @@
+<style>
+
+</style>
+<template>
+    <md-list-item @click="gotoRadio">
+        <md-avatar>
+            <img alt="radio.name" :src="radio.logo_url">
+        </md-avatar>
+        <div class="md-list-text-container">
+            <span>{{radio.name}} - {{radio.description}}</span>
+            <p>
+                <flag-icon :language="radio.language.iso" />
+            </p>
+        </div>
+    </md-list-item>
+</template>
+<script>
+    import FlagIcon from '../../common/FlagIcon';
+
+    export default {
+        name: "radio-list-item",
+        components: {
+            FlagIcon
+        },
+        props: {
+            radio: Object
+        },
+        methods: {
+            gotoRadio (){
+                this.$router.push({ name: 'singleRadioView', params: { radioId: this.radio.id  }});
+            }
+        }
+    }
+
+</script>
