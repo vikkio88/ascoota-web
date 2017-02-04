07299506957df6a54b4ad69a7176a8e7c8b21c48
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-04 11:29:31 +0000

    fixed About layout with material

--- src/components/main/About.vue
+++ src/components/main/About.vue
@@ -4,62 +4,46 @@
     }
 </style>
 <template>
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <h3 class="modal-title">About aScoota</h3>
-            <h4>Made with ♥ by <a href="http://www.vikkio.it" target="_blank">vikkio</a></h4>
-            <p><strong>Ascoota</strong> is a small side project of mine, it will be a podcast player/manager.</p>
-            <p>At the moment I am planning just to show some free random podcasts. Some of them are from my old radio show
-                <a href="https://www.facebook.com/BazingaRadioSISMA" title="Bazinga!" target="_blank">Bazinga!</a> and some
-                from italian Radio.</p>
-            <p>Soon I will add the option to create a custom library by adding your personal RSS feeds.</p>
+    <md-card>
+        <md-card-header>
+            <div class="md-title">About aScoota</div>
+            <div class="md-subhead">Made with ♥ by <a href="http://www.vikkio.it" target="_blank">vikkio</a></div>
+        </md-card-header>
+        <md-card-content>
+            <strong>Ascoota</strong> is a small side project of mine, it will be a podcast player/manager. At the moment
+            I am planning just to show some free random podcasts. Some of them are from my old radio show
+            <a href="https://www.facebook.com/BazingaRadioSISMA" title="Bazinga!" target="_blank">Bazinga!</a> and some from
+            italian Radio. Soon I will add the option to create a custom library by adding your personal RSS feeds.
             <p>I will always keep everything open-source, and if you are a computer geek and you want to collaborate with this
                 project just check out those links:
                 <md-table>
                     <md-table-body>
-                        <md-table-row>
-                            <md-button class="md-raised md-primary">
-                                <a target="_blank" href="https://github.com/vikkio88/ascoota-web">
-                                aScootaWeb (vuejs 2, ES6)
-                                </a>
-                            </md-button>
+                        <a target="_blank" href="https://github.com/vikkio88/ascoota-web">aScootaWeb (vuejs 2, ES6)</a>
                         </md-table-row>
                         <md-table-row>
-                            <md-button class="md-raised md-primary">
-                                <a target="_blank" href="https://github.com/vikkio88/ascoota-api">aScootaBE (php, slime)</a>
-                            </md-button>
+                            <a target="_blank" href="https://github.com/vikkio88/ascoota-api">aScootaBE (php, slime)</a>
                         </md-table-row>
                         <md-table-row>
-                            <md-button class="md-raised md-warn">
-                                <a target="_blank" href="https://github.com/vikkio88/slime">SlimeApi (backend rest api)</a>
-                            </md-button>
+                            <a target="_blank" href="https://github.com/vikkio88/slime">SlimeApi (backend rest api)</a>
                         </md-table-row>
                         <md-table-row>
-                            <md-button class="md-raised md-warn">
-                                <a target="_blank" href="https://www.slimframework.com/">Slim 3</a>
-                            </md-button>
+                            <a target="_blank" href="https://www.slimframework.com/">Slim 3</a>
                         </md-table-row>
                         <md-table-row>
-                            <md-button class="md-raised md-warn">
-                                <a target="_blank" href="https://github.com/illuminate/database">Eloquent</a>
-                            </md-button>
+                            <a target="_blank" href="https://github.com/illuminate/database">Eloquent</a>
                         </md-table-row>
                         <md-table-row>
-                            <md-button class="md-raised md-warn">
-                                <a target="_blank" href="https://vuematerial.github.io/">Vue-material</a>
-                            </md-button>
+                            <a target="_blank" href="https://vuematerial.github.io/">Vue-material</a>
                         </md-table-row>
                         <md-table-row>
-                            <md-button class="md-raised md-warn">
-                                <a target="_blank" href="https://webpack.github.io/">Webpack (js module bundler)</a>
-                            </md-button>
+                            <a target="_blank" href="https://webpack.github.io/">Webpack (js module bundler)</a>
                         </md-table-row>
 
                     </md-table-body>
                 </md-table>
             </p>
-        </div>
-    </div>
+        </md-card-content>
+    </md-card>
 </template>
 <script>
     export default {
