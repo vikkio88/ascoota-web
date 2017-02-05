2cc4df1f5086c29b368bdf1358936ddf25a36c0d
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-05 12:48:10 +0000

    moved single show to vue-material

--- src/components/podcast/Show.vue
+++ src/components/podcast/Show.vue
@@ -1,5 +1,41 @@
 <style></style>
 <template>
+    <div>
+        <md-card>
+            <md-card-header>
+                <router-link :to="{ name: 'singleRadioView', params: { radioId: radioId }}">
+                    <md-icon>arrow_back</md-icon>
+                </router-link>
+                <md-card-media>
+                    <img class="img" :alt="show.name" :title="show.name" :src="show.logo_url" />
+                </md-card-media>
+                <md-card-header-text>
+                    <div class="md-title">{{show.name}}
+                        <flag-icon :language="language" />
+                    </div>
+                </md-card-header-text>
+            </md-card-header>
+            <md-card-content>
+                {{show.description}}
+            </md-card-content>
+            <md-card-actions>
+                <a :href="show.website" target="_blank">
+                    <md-icon>link</md-icon>
+                </a>
+                <a :href="show.feed_url" target="_blank">
+                    <md-icon>rss_feed</md-icon>
+                </a>
+            </md-card-actions>
+        </md-card>
+        <md-card>
+            <md-card-content v-if="podcasts.length">
+                <md-list class="md-double-line">
+                    <podcast-list-item :podcast="podcast" :showimg="false" v-for="podcast in podcasts" />
+                </md-list>
+            </md-card-content>
+        </md-card>
+    </div>
+    <!--
     <div class="panel panel-primary">
         <img class="img" :alt="show.name" :title="show.name" :src="show.logo_url" />
         <div class="panel-heading">
@@ -38,9 +74,10 @@
             </div>
         </div>
     </div>
+    -->
 </template>
 <script>
-    import PodcastCard from './cards/PodcastCard'
+    import PodcastListItem from './listItems/Podcast'
     import ShowService from '../../services/ascoota/ShowService.js'
     import FlagIcon from '../common/FlagIcon'
 
@@ -48,7 +85,7 @@
         name: 'show',
         components: {
             FlagIcon,
-            PodcastCard
+            PodcastListItem
         },
         data() {
             return {
@@ -83,8 +120,9 @@
                     (error) => {
                         console.log(error);
                     }
-                );
+                    );
             }
         }
     }
+
 </script>
