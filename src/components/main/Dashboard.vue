1bceb46193b47914ae137d7aa25a6ea73f1c84e3
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-02 22:24:14 +0000

    adding shows widget

--- src/components/main/Dashboard.vue
+++ src/components/main/Dashboard.vue
@@ -6,18 +6,21 @@
 <template>
     <div id="dashboard">
         <div class="panel-group">
+            <shows />
             <radios />
             <podcasts />
         </div>
     </div>
 </template>
 <script>
+    import Shows from './dashboard/Shows'
     import Radios from './dashboard/Radios'
     import Podcasts from './dashboard/Podcasts'
 
     export default {
         name: "dashboard",
         components: {
+            Shows,
             Radios,
             Podcasts
         }
