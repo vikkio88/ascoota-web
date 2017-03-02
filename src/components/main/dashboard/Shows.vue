f2c870fd3838e83c274609a5013067d90fa6707c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-02 22:00:09 +0000

    adding show home widget

---
+++ src/components/main/dashboard/Shows.vue
@@ -0,0 +1,49 @@
+<style>
+</style>
+<template>
+    <md-card v-if="shows.length > 0">
+        <md-card-header>
+            <md-card-header-text>
+                <md-icon>trending_up</md-icon>
+                <md-icon>headset</md-icon>
+            </md-card-header-text>
+        </md-card-header>
+        <div class="shows-wrapper">
+            <md-card-content>
+                <md-layout md-row md-align="center">
+                <radio-card :show="show" v-for="show in shows" />
+                </md-layout>
+            </md-card-content>
+        </div>
+    </md-card>
+</template>
+<script>
+    import RadioCard from '../../podcast/cards/RadioCard'
+    import RadioService from '../../../services/ascoota/RadioService'
+
+    var radioService = new RadioService();
+
+    export default {
+        name: "showsWidget",
+        mounted() {
+            radioService.getAll().then(
+                (data) => {
+                    this.radios = data.body.payload;
+                }
+            ).catch(
+                (errors) => {
+                    console.log(errors)
+                }
+                );
+        },
+        data() {
+            return {
+                radios: []
+            };
+        },
+        components: {
+            RadioCard
+        }
+    }
+
+</script>
