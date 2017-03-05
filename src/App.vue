c83d8dbea3e5c43e8abc631bce83fdfd1d98ea5f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-05 20:54:47 +0000

    moving audioplayer visual to its component

--- src/App.vue
+++ src/App.vue
@@ -22,23 +22,7 @@
         <router-view></router-view>
       </md-card-content>
     </md-card>
-
-    <md-speed-dial v-if="audioSelected" md-mode="fling" md-direction="top" class="md-fab md-fab-bottom-right" md-theme="speedDial">
-      <md-button class="md-fab" md-fab-trigger>
-        <md-icon md-icon-morph>close</md-icon>
-        <md-icon>audiotrack</md-icon>
-      </md-button>
-      <md-button @click.native="togglePlayer" class="md-fab md-mini md-clean">
-        <md-icon>info_outline</md-icon>
-      </md-button>
-      <md-button class="md-fab md-mini md-clean">
-        <md-icon>play_arrow</md-icon>
-      </md-button>
-    </md-speed-dial>
-
-    <md-sidenav class="md-right" ref="rightSidenav">
-      <audio-player/>
-    </md-sidenav>
+    <audio-player />
   </div>
 </template>
 
@@ -61,11 +45,6 @@
     },
     mounted() {
       this.$stats.push('render_home');
-    },
-    methods: {
-      togglePlayer() {
-        this.$refs.rightSidenav.toggle();
-      }
     }
   }
 
