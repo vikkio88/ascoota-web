553dba4d7631781767ffc3ec7452f8a93f67039b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-09 21:45:30 +0000

    adding info of show inside podcast list item

--- src/components/podcast/listItems/Podcast.vue
+++ src/components/podcast/listItems/Podcast.vue
@@ -45,7 +45,9 @@
                 if (!this.isSelected) {
                     if (this.show !== undefined) {
                         this.podcast.show = {
-                            logo_url: this.show.logo_url
+                            name: this.show.name,
+                            logo_url: this.show.logo_url,
+                            slug: this.show.slug
                         }
                     }
                     this.$store.state.selectedAudio = this.podcast;
