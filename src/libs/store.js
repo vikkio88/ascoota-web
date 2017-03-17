a998da3624ab938ff4ccf025633e89ebef42a449
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-17 21:24:32 +0000

    setting up store mutation to handle user login

--- src/libs/store.js
+++ src/libs/store.js
@@ -3,13 +3,41 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
+const LOGIN = "LOGIN";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGOUT = "LOGOUT";
+
 
 const store = new Vuex.Store({
-    state:{
+    state: {
         selectedAudio: undefined,
         autoPlay: false,
         showMainDialog: false,
-        startTime: 0
+        startTime: 0,
+        isLoggedIn: !!localStorage.getItem("token")
+    },
+    mutations: {
+        [LOGIN](state) {
+            state.pending = true;
+        },
+        [LOGIN_SUCCESS](state) {
+            state.isLoggedIn = true;
+            state.pending = false;
+        },
+        [LOGOUT](state) {
+            state.isLoggedIn = false;
+        }
+    },
+    actions: {
+        login({ commit }, token) {
+            commit(LOGIN);
+            localStorage.setItem("token", token);
+            commit(LOGIN_SUCCESS);
+        },
+        logout({ commit }) {
+            localStorage.removeItem("token");
+            commit(LOGOUT);
+        }
     }
 });
 
