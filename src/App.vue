4b16f18703e812a738c81e0173dd8b75c8525266
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-19 17:31:21 +0000

    removing wrapper card

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,40 @@
 <style>
-  html,
-  body,
-  .app-viewport {
-    height: 100%;
-    overflow: hidden;
-    text-align: center;
-  }
-  
-  .app-viewport {
-    display: flex;
-    flex-flow: column;
-  }
+html,
+body,
+.app-viewport {
+  height: 100%;
+  overflow: hidden;
+  text-align: center;
+}
+
+.app-viewport {
+  display: flex;
+  flex-flow: column;
+}
 </style>
 <template>
   <div id="app" class="app-viewport phone-viewport">
     <main-nav />
-    <md-card>
-      <md-card-content>
-        <router-view></router-view>
-      </md-card-content>
-    </md-card>
+    <router-view></router-view>
     <audio-player />
   </div>
 </template>
 
 <script>
-  import AudioPlayer from './components/main/AudioPlayer'
-  import MainNav from './components/navbars/MainNav'
-  import Dashboard from './components/main/Dashboard'
+import AudioPlayer from './components/main/AudioPlayer'
+import MainNav from './components/navbars/MainNav'
+import Dashboard from './components/main/Dashboard'
 
-  export default {
-    name: 'app',
-    components: {
-      AudioPlayer,
-      MainNav,
-      Dashboard
-    },
-    mounted() {
-      this.$stats.push('render_home');
-    }
+export default {
+  name: 'app',
+  components: {
+    AudioPlayer,
+    MainNav,
+    Dashboard
+  },
+  mounted() {
+    this.$stats.push('render_home');
   }
+}
 
 </script>
