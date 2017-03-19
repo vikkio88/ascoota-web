85d943323d0a7a149034567ce46e7d4b73cc6bc0
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-19 17:29:03 +0000

    implementing tabs for dashboard

--- src/components/main/Dashboard.vue
+++ src/components/main/Dashboard.vue
@@ -1,28 +1,41 @@
 <style>
-    .card {
-        margin-top: 5px;
-    }
+.card {
+    margin-top: 5px;
+}
 </style>
 <template>
     <div id="dashboard">
         <div class="panel-group">
-            <shows />
-            <radios />
-            <podcasts />
+            <md-tabs md-centered>
+                <md-tab md-label="Shows"
+                        md-icon="ondemand_video">
+                    <shows />
+                </md-tab>
+    
+                <md-tab md-label="Radios"
+                        md-icon="radios">
+                    <radios />
+                </md-tab>
+    
+                <md-tab md-label="Podcasts"
+                        md-icon="audiotrack">
+                    <podcasts />
+                </md-tab>
+            </md-tabs>
         </div>
     </div>
 </template>
 <script>
-    import Shows from './dashboard/Shows'
-    import Radios from './dashboard/Radios'
-    import Podcasts from './dashboard/Podcasts'
+import Shows from './dashboard/Shows'
+import Radios from './dashboard/Radios'
+import Podcasts from './dashboard/Podcasts'
 
-    export default {
-        name: "dashboard",
-        components: {
-            Shows,
-            Radios,
-            Podcasts
-        }
+export default {
+    name: "dashboard",
+    components: {
+        Shows,
+        Radios,
+        Podcasts
     }
+}
 </script>
