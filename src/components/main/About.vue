6ee28d45e9c7ebecd31d93ae783b4e6b8e46d0fe
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-19 13:11:24 +0000

    moved info and dev links to about page

--- src/components/main/About.vue
+++ src/components/main/About.vue
@@ -10,10 +10,23 @@
             <div class="md-subhead">Made with ♥ by <a href="http://www.vikkio.it" target="_blank">vikkio</a></div>
         </md-card-header>
         <md-card-content>
-            <strong>Ascoota</strong> is a small side project of mine, it will be a podcast player/manager. At the moment
+            <p><strong>Ascoota</strong> is a small side project of mine, it will be a podcast player/manager. At the moment
             I am planning just to show some free random podcasts. Some of them are from my old radio show
             <a href="https://www.facebook.com/BazingaRadioSISMA" title="Bazinga!" target="_blank">Bazinga!</a> and some from
             italian Radio. Soon I will add the option to create a custom library by adding your personal RSS feeds.
+            <a href="https://github.com/vikkio88/ascoota-web" target="_blank">
+                <md-button class="md-raised md-warn">
+                        <img src="../../assets/github-circle.png" alt="github"> codebase
+                </md-button>
+            </a>
+            <md-button class="md-raised md-warn" style="color:white">
+                <a target="_blank" href="https://github.com/vikkio88/ascoota-web/issues/new">
+                <span style="color:white">
+                    <md-icon>bug_report</md-icon> Report bug
+                </span>
+                </a>
+            </md-button>
+            </p>
             <p>I will always keep everything open-source, and if you are a computer geek and you want to collaborate with this
                 project just check out those links:
                 <md-table>
