7f0aa94af53d9ebb6526bec16dafc4513ee666b4
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-19 13:39:34 +0000

    fixed and improved about page

--- src/components/main/About.vue
+++ src/components/main/About.vue
@@ -1,66 +1,97 @@
 <style scoped>
-    a {
-        color: black;
-    }
+a {
+    color: black;
+}
 </style>
 <template>
     <md-card>
         <md-card-header>
             <div class="md-title">About aScoota</div>
-            <div class="md-subhead">Made with ♥ by <a href="http://www.vikkio.it" target="_blank">vikkio</a></div>
+            <div class="md-subhead">Made with ♥ by <a href="http://www.vikkio.it"
+                   target="_blank">vikkio</a></div>
         </md-card-header>
         <md-card-content>
-            <p><strong>Ascoota</strong> is a small side project of mine, it will be a podcast player/manager. At the moment
-            I am planning just to show some free random podcasts. Some of them are from my old radio show
-            <a href="https://www.facebook.com/BazingaRadioSISMA" title="Bazinga!" target="_blank">Bazinga!</a> and some from
-            italian Radio. Soon I will add the option to create a custom library by adding your personal RSS feeds.
-            <a href="https://github.com/vikkio88/ascoota-web" target="_blank">
-                <md-button class="md-raised md-warn">
-                        <img src="../../assets/github-circle.png" alt="github"> codebase
+        <div><md-button class="md-raised md-warn">
+                    <a href="https://github.com/vikkio88/ascoota-web"
+                       target="_blank" style="color:white">
+                        <img src="../../assets/github-circle.png"
+                             alt="github"> codebase
+                    </a>
                 </md-button>
-            </a>
-            <md-button class="md-raised md-warn" style="color:white">
-                <a target="_blank" href="https://github.com/vikkio88/ascoota-web/issues/new">
-                <span style="color:white">
-                    <md-icon>bug_report</md-icon> Report bug
-                </span>
-                </a>
-            </md-button>
+    
+                <md-button class="md-raised md-warn"
+                           style="color:white">
+                    <a target="_blank"
+                       href="https://github.com/vikkio88/ascoota-web/issues/new">
+                        <span style="color:white">
+                                <md-icon>bug_report</md-icon> Report bug
+                            </span>
+                    </a>
+                </md-button></div>
+            <p><strong>Ascoota</strong> is a small side project of mine, it will be a podcast player/manager. At the moment I am planning just to show some free random podcasts. Some of them are from my old radio show
+                <a href="https://www.facebook.com/BazingaRadioSISMA"
+                   title="Bazinga!"
+                   target="_blank">Bazinga!</a> and some from italian Radio. Soon I will add the option to create a custom library by adding your personal RSS feeds.
             </p>
-            <p>I will always keep everything open-source, and if you are a computer geek and you want to collaborate with this
-                project just check out those links:
-                <md-table>
-                    <md-table-body>
-                        <a target="_blank" href="https://github.com/vikkio88/ascoota-web">aScootaWeb (vuejs 2, ES6)</a>
-                        </md-table-row>
-                        <md-table-row>
-                            <a target="_blank" href="https://github.com/vikkio88/ascoota-api">aScootaBE (php, slime)</a>
-                        </md-table-row>
-                        <md-table-row>
-                            <a target="_blank" href="https://github.com/vikkio88/slime">SlimeApi (backend rest api)</a>
-                        </md-table-row>
-                        <md-table-row>
-                            <a target="_blank" href="https://www.slimframework.com/">Slim 3</a>
-                        </md-table-row>
-                        <md-table-row>
-                            <a target="_blank" href="https://github.com/illuminate/database">Eloquent</a>
-                        </md-table-row>
-                        <md-table-row>
-                            <a target="_blank" href="https://vuematerial.github.io/">Vue-material</a>
-                        </md-table-row>
-                        <md-table-row>
-                            <a target="_blank" href="https://webpack.github.io/">Webpack (js module bundler)</a>
-                        </md-table-row>
-
-                    </md-table-body>
-                </md-table>
+            <p>I will always keep everything open-source, and if you are a computer geek and you want to collaborate with this project just check out those links:
+                <md-layout md-row
+                           md-align="center">
+                    <dev-card :item="item"
+                              v-for="item in items" />
+                </md-layout>
             </p>
         </md-card-content>
     </md-card>
 </template>
 <script>
-    export default {
-        name: 'about'
+import DevCard from './devlinks/DevCard'
+
+export default {
+    name: 'about',
+    components: {
+        DevCard
+    },
+    data() {
+        return {
+            items: [
+                {
+                    name: 'aScootaWeb',
+                    tech: 'vue2, ES6',
+                    link: 'https://github.com/vikkio88/ascoota-web'
+                },
+                {
+                    name: 'aScootaApi',
+                    tech: 'php, slime',
+                    link: 'https://github.com/vikkio88/ascoota-api'
+                },
+                {
+                    name: 'SlimeApi',
+                    tech: 'php, slim, backend rest api',
+                    link: 'https://github.com/vikkio88/slime'
+                },
+                {
+                    name: 'Slim3',
+                    tech: 'php microframework',
+                    link: 'https://www.slimframework.com/'
+                },
+                {
+                    name: 'Eloquent',
+                    tech: 'php ORM from Laravel',
+                    link: 'https://github.com/illuminate/database'
+                },
+                {
+                    name: 'Vue-Material',
+                    tech: 'material design components for vue2',
+                    link: 'https://vuematerial.github.io/'
+                },
+                {
+                    name: 'Webpack',
+                    tech: 'js module bundler',
+                    link: 'https://webpack.github.io/'
+                },
+            ]
+        }
     }
+}
 
 </script>
---
+++ src/components/main/devlinks/DevCard.vue
@@ -0,0 +1,20 @@
+<style>
+</style>
+<template>
+    <md-card md-with-hover>
+        <a :href="item.link" target="_blank">
+            <md-card-header>
+                <div class="md-title">{{item.name}}</div>
+                <div class="md-subhead">{{item.tech}}</div>
+            </md-card-header>
+        </a>
+    </md-card>
+</template>
+<script>
+export default {
+    name: 'devCard',
+    props: {
+        item: { type: Object, default: (() => { }) }
+    }
+}
+</script>
