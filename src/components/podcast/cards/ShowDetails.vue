db41b2424a700055e4c47bd24b9c52b756e2af94
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-03-27 21:05:45 +0100

    adding authors to show view and fixed unclickable links #38

--- src/components/podcast/cards/ShowDetails.vue
+++ src/components/podcast/cards/ShowDetails.vue
@@ -4,54 +4,63 @@
         <md-card>
             <md-card-header>
                 <md-card-media>
-                    <img class="img" :alt="show.name" :title="show.name" :src="show.logo_url" />
+                    <img class="img"
+                         :alt="show.name"
+                         :title="show.name"
+                         :src="show.logo_url" />
                 </md-card-media>
                 <md-card-header-text>
                     <div class="md-title">{{show.name}}
                         <flag-icon :language="language" />
                     </div>
+                    <div class="md-subhead">{{show.author}}</div>
                 </md-card-header-text>
             </md-card-header>
             <md-card-content>
-                {{show.description}}
+                <div>{{show.description}}</div>
             </md-card-content>
-            <md-card-actions>
-                <a :href="show.website" target="_blank">
+            <div>
+                <a :href="show.website"
+                   target="_blank">
                     <md-icon>link</md-icon>
                 </a>
-                <a :href="show.feed_url" target="_blank">
+                <a :href="show.feed_url"
+                   target="_blank">
                     <md-icon>rss_feed</md-icon>
                 </a>
-            </md-card-actions>
+            </div>
         </md-card>
         <md-card>
             <md-card-content v-if="podcasts.length">
                 <md-list class="md-double-line">
-                    <podcast-list-item :podcast="podcast" :show="show" :showimg="false" v-for="podcast in podcasts" />
+                    <podcast-list-item :podcast="podcast"
+                                       :show="show"
+                                       :showimg="false"
+                                       v-for="podcast in podcasts" />
                 </md-list>
             </md-card-content>
         </md-card>
     </div>
 </template>
 <script>
-    import PodcastListItem from '../listItems/Podcast'
-    import FlagIcon from '../../common/FlagIcon'
+import PodcastListItem from '../listItems/Podcast'
+import FlagIcon from '../../common/FlagIcon'
 
-    export default {
-        name: "showDetails",
-        components: {
-            PodcastListItem,
-            FlagIcon
-        },
-        props: {
-            show: Object,
-            podcasts: { type: Array, default: [] },
-            language: { type: String, default: "it" },
-            page: { type: Number, default: 1 }
-        },
-        methods: {
+export default {
+    name: "showDetails",
+    components: {
+        PodcastListItem,
+        FlagIcon
+    },
+    props: {
+        show: Object,
+        podcasts: { type: Array, default: [] },
+        language: { type: String, default: "it" },
+        page: { type: Number, default: 1 }
+    },
+    methods: {
 
-        }
     }
+}
 
 </script>
