ec779c06d7ada8613490848630e8e595a44561a7
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-04-09 10:42:46 +0100

    using new commit to trigger podcast

--- src/components/podcast/PodcastTrigger.vue
+++ src/components/podcast/PodcastTrigger.vue
@@ -6,40 +6,37 @@
     </div>
 </template>
 <script>
-    import PodcastService from '../../services/ascoota/PodcastService'
+import PodcastService from '../../services/ascoota/PodcastService'
 
-    export default {
-        name: 'podcastTrigger',
-        data() {
-            return {
-                params: this.$route.params,
-                time: this.$route.query.t,
-                podcastId: this.$route.params.podcastId
-            };
-        },
-        mounted() {
-            this.init();
-        },
-        methods: {
-            init() {
-                let service = new PodcastService();
-                service.getOne(this.podcastId).then(
-                    (data) => {
-                        if(data.body.payload !== null){
-                            this.$store.state.selectedAudio = data.body.payload;
-                            this.$store.state.autoPlay = true;
-                            if (this.time != undefined) {
-                                this.$store.state.startTime = this.time;
-                            }
-                        }
-                        this.$router.push({ name: 'dashboard' });
+export default {
+    name: 'podcastTrigger',
+    data() {
+        return {
+            params: this.$route.params,
+            time: this.$route.query.t,
+            podcastId: this.$route.params.podcastId
+        };
+    },
+    mounted() {
+        this.init();
+    },
+    methods: {
+        init() {
+            let service = new PodcastService();
+            service.getOne(this.podcastId).then(
+                (data) => {
+                    if (data.body.payload !== null) {
+                        let podcast = data.body.payload;
+                        this.$store.commit('selectAndPlay', { podcast, initialSeek: this.$route.query.t })
                     }
-                ).catch(
-                    (error) => {
-                        console.log(error);
-                    }
+                    this.$router.push({ name: 'dashboard' });
+                }
+            ).catch(
+                (error) => {
+                    console.log(error);
+                }
                 );
-            }
         }
     }
+}
 </script>
