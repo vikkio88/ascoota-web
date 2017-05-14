b8db1d34ad335c36cafa4adf49e87f63ee281c7b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-05-14 17:16:28 +0100

    add expiration to Auth plugin

--- src/plugins/Auth.js
+++ src/plugins/Auth.js
@@ -1,22 +1,29 @@
 const TOKEN_KEY = process.env.TOKEN_LOCAL_KEY;
-console.log(TOKEN_KEY);
+const TOKEN_EXPIRATION = 'token_expiration';
+
 const Auth = {
     setToken(token) {
+        let utc_now = Date.now() / 1000 | 0;
         localStorage.setItem(TOKEN_KEY, token);
+        localStorage.setItem(TOKEN_EXPIRATION, utc_now + 23 * 3600)
     },
     destroyToken() {
         localStorage.removeItem(TOKEN_KEY);
+        localStorage.removeItem(TOKEN_EXPIRATION);
     },
     getToken() {
+        let utc_now = Date.now() / 1000 | 0;
         let token = localStorage.getItem(TOKEN_KEY);
-        if (!token) {
+        let expiration = localStorage.getItem(TOKEN_EXPIRATION);
+        if (!token || !expiration || utc_now > parseInt(expiration)) {
+            this.destroyToken();
             return null;
         }
+
         return token;
     },
     loggedIn() {
         return this.getToken() ? true : false;
-
     }
 }
 
